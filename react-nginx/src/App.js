5b9011131448fbf8dce670022dd83d693f9f757c
alastairwoods <alastair.woods@exceptionuk.com>
2023-06-20 11:57:58 +0100

    text change homepage

--- react-nginx/src/App.js
+++ react-nginx/src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and AW save to reload.
+        </p>
+        <p>
+          Text modificattions AW 20-06-23.
         </p>
         <a
           className="App-link"
